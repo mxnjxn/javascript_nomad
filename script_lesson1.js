7d6791bfbb52647b5780d9faa9be21ec88c80a6e
mxnjxn <choe86898@gmail.com>
2022-03-03 00:10:38 +0900

    ..

---
+++ script_lesson1.js
@@ -0,0 +1,30 @@
+//console is where the javascript is going to communicate
+alert("hi");
+
+// print something
+console.log(); // able to put numbers and strings
+console.log(234223423);
+console.log("afasfas") 
+
+//declaring variable. Use const(short for constant) as prefix. 
+// const does not change. Can't assign new variable to const
+const a = 10;
+const b = 2;
+const myName = "nico"; // conventionally, javascript use the camel case.
+
+// Other way to declare variables. Use prefix "let" instead of "const" 
+// Use let for the first declaration. Does not have to use it twice when modifing the value.
+let yourName = "Tony"
+yourName = "Minjun"
+
+//
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("Hello " + myName);
+console.log("Hello, your name is " + yourName);
+
+// SUMMARY
+// let : not able to redeclare , able to re-assign different value
+// const: not able to redelcare, not able to re-assign different value
+// var: able to redeclare, able to re-assign different value.
