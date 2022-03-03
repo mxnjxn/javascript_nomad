3897230f16720a19cc8d9239fe01122926ba0f23
mxnjxn <choe86898@gmail.com>
2022-03-03 12:35:22 +0900

    ..

--- script_lesson4.js
+++ script_lesson4.js
@@ -3,7 +3,7 @@ const playerPoints = 121212;
 const playerHandsome = false;
 const playerFat = "little bit"
 
-//
+//Array
 const player = ["nico", 121212, false, "little bit"];
 
 // object--> have many properties
@@ -14,11 +14,23 @@ const player2 = {
 };
 
 //console is an object
-console.log(player2);
+console.log(player2); 
 console.log(player2.name);
 console.log(player2["fat"]);
+console.log(player2["points"]); 
+//In an array type variable you can access to specific data using index such as [0,1,2]..
+// However, in the object type variable, you can access to the specific data using "string" in the index [] such as ["name"] as opposed to the array type which use number in index [].
+console.log(player2[1]);  //undefined --> 
+console.log(player2.fat);
 
 //add item to the object
 console.log("*******")
-player.fat = false;
+// const cannot be re-assigned to a new value. However, it is able to change the inner properties of a const when const is an object. 
+player2.fat = false;
 console.log(player2);
+
+player2.lastName= "potato";
+console.log(player2);
+
+player2.points = player2.points + 14;
+console.log(player2.points);
